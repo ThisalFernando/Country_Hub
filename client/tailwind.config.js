0e58b1a2ac698e21da9a3bb89461492981f9cf49
thisal <thisalfernando39@gmail.com>
2025-05-01 23:58:18 +0530

    Rest countries client side - tailwind.config.js

---
+++ client/tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}",],
+  theme: {
+    extend: {
+      animation: {
+        fadeInUp: 'fadeInUp 1s ease-out',
+      },
+      
+      keyframes: {
+        fadeInUp: {
+          '0%': { opacity: 0, transform: 'translateY(50px)' },
+          '100%': { opacity: 1, transform: 'translateY(0)' },
+        },
+      },
+
+      fontFamily: {
+        heading: ['Poppins', 'sans-serif'],
+        body: ['Roboto', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
