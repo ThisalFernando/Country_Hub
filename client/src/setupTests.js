5cca5a6bfdff64a703ac09fd660b15e66f05de97
thisal <thisalfernando39@gmail.com>
2025-05-03 02:49:29 +0530

    Rest countries client side - setupTests.js

---
+++ client/src/setupTests.js
@@ -0,0 +1,17 @@
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom
+import '@testing-library/jest-dom';
+// Polyfill for TextEncoder
+import { TextEncoder, TextDecoder } from 'util';
+
+if (typeof global.TextEncoder === 'undefined') {
+  global.TextEncoder = TextEncoder;
+}
+
+if (typeof global.TextDecoder === 'undefined') {
+  global.TextDecoder = TextDecoder;
+}
+
+
