5de566839e9fef3f69c555e8312a8429d455cf21
thisal <thisalfernando39@gmail.com>
2025-05-03 21:44:24 +0530

    Rest countries server side - updated index.js

--- server/index.js
+++ server/index.js
@@ -3,7 +3,6 @@ import dotenv from "dotenv";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
-import serverless from "serverless-http";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 import favoriteRoutes from "./routes/favoriteRoutes.js";
@@ -31,9 +30,6 @@ app.get("/", (req, res) => {
 app.use("/api/auth", authRoutes);
 app.use("/api/favorites", favoriteRoutes);
 
-/*
+
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
-*/
-
-export default serverless(app);
